5833e80cd52d82cf8de412779e504c11faff4e98
Maria Anderson <Mandersonreka@gmail.com>
2023-01-03 10:10:15 -0700

    authentication

--- src/components/contexts/user.context.jsx
+++ src/components/contexts/user.context.jsx
@@ -1,13 +1,15 @@
-// import { createContext, useState } from "react";
+import { createContext, useState } from 'react'
 
-// export const UserContext = createContext({
-//   setCurrentUser: () => null,
-//   currentUser: null,
-// });
+//as the actual value you want to access
+export const UserContext = createContext({
+    currentUser: null,
+    setCurrentUser: () => null,
+});
 
-// export const UserProvider = ({ children }) => {
-//   const [currentUser, setCurrentUser] = useState(null);
-//   const value = { currentUser, setCurrentUser };
+export const UserProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(null);
+    const value = { currentUser, setCurrentUser };
 
-//   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
-// };
+
+    return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+};
